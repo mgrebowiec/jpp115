edc098a71464590ba99e80a325ebd9dea95223f5
mgrebowiec <michal.grebowiec@gmail.com>
2022-05-14 09:31:10 +0200

    Dodanie metody sayHello

--- src/pl/sda/mg/jpp115/Hello.java
+++ src/pl/sda/mg/jpp115/Hello.java
@@ -3,5 +3,10 @@ package pl.sda.mg.jpp115;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Udalo sie zaciagnac projekt z GitHuba!");
+        sayHello("Michal");
+    }
+
+    private static void sayHello(String name) {
+        System.out.printf("Witaj, %s", name);
     }
 }
